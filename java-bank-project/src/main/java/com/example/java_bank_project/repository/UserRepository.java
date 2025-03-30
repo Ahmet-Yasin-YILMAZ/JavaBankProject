6583314b86aece1f6dd870c76d5055ebc58ce8f3
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-03-31 00:14:52 +0300

    Updated UserRepository.java

--- java-bank-project/src/main/java/com/example/java_bank_project/repository/UserRepository.java
+++ java-bank-project/src/main/java/com/example/java_bank_project/repository/UserRepository.java
@@ -4,5 +4,5 @@ import com.example.java_bank_project.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    Boolean existsByEmail(String email);
+    boolean existsByEmail(String email);
 }
