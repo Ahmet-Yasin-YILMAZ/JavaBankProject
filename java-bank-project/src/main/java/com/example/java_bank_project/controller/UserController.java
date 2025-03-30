1a94c0edc9bfb6bd82b8663fd276562cfd2e27b0
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-03-31 00:14:41 +0300

    Updated UserController.java

--- java-bank-project/src/main/java/com/example/java_bank_project/controller/UserController.java
+++ java-bank-project/src/main/java/com/example/java_bank_project/controller/UserController.java
@@ -3,17 +3,20 @@ package com.example.java_bank_project.controller;
 import com.example.java_bank_project.dto.BankResponse;
 import com.example.java_bank_project.dto.UserRequest;
 import com.example.java_bank_project.service.impl.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
-    @Autowired
-    UserService userService;
+
+    private final UserService userService;
+
 
     @PostMapping
     public BankResponse createAccount(@RequestBody UserRequest userRequest){
