1f0661d494ce3f69753af8fe4e733ce1f91ff72b
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-03-31 22:15:00 +0300

    Added EmailServiceImpl.java

---
+++ java-bank-project/src/main/java/com/example/java_bank_project/service/impl/EmailServiceImpl.java
@@ -0,0 +1,39 @@
+package com.example.java_bank_project.service.impl;
+
+import com.example.java_bank_project.dto.EmailDetails;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService{
+
+    private JavaMailSender javaMailSender;
+
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+
+
+    @Override
+    public void sendEmailAlert(EmailDetails emailDetails) {
+        try{
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setText(emailDetails.getMessageBody());
+            mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            System.out.println("Mail sent successfully");
+        } catch (MailException e){
+            throw new RuntimeException(e);
+        }
+
+    }
+}
