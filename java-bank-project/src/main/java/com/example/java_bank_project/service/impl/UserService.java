3ec4154f7851e33598160e445b9a4d8923d3554d
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-04-03 17:52:06 +0300

    Added UserService.java

--- java-bank-project/src/main/java/com/example/java_bank_project/service/impl/UserService.java
+++ java-bank-project/src/main/java/com/example/java_bank_project/service/impl/UserService.java
@@ -1,8 +1,14 @@
 package com.example.java_bank_project.service.impl;
 
 import com.example.java_bank_project.dto.BankResponse;
+import com.example.java_bank_project.dto.CreditDebitRequest;
+import com.example.java_bank_project.dto.EnquiryRequest;
 import com.example.java_bank_project.dto.UserRequest;
 
 public interface UserService {
     BankResponse createAccount(UserRequest userRequest);
+    BankResponse balanceEnquiry(EnquiryRequest request);
+    String nameEnquiry(EnquiryRequest request);
+    BankResponse creaditAccount(CreditDebitRequest request);
+    BankResponse debitAccount(CreditDebitRequest request);
 }
