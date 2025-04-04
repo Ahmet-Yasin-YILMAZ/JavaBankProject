f7f9bdb795f45bc0d45d2858b0890f6644ab116b
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-04-04 22:04:41 +0300

    Updated JavaBankProjectApplication.java

--- java-bank-project/src/main/java/com/example/java_bank_project/JavaBankProjectApplication.java
+++ java-bank-project/src/main/java/com/example/java_bank_project/JavaBankProjectApplication.java
@@ -1,9 +1,34 @@
 package com.example.java_bank_project;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "The Java Bank Project",
+				description = "Backend Rest APIs for Java Bank Project",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Ahmet Yasin YILMAZ",
+						email = "ylmzayasin@gmail.com",
+						url = "https://github.com/Ahmet-Yasin-YILMAZ/java-bank-project"
+				),
+				license = @License(
+						name = "The Java Bank Project",
+						url = "https://github.com/Ahmet-Yasin-YILMAZ/java-bank-project"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Java Bank Project",
+                url = "https://github.com/Ahmet-Yasin-YILMAZ/java-bank-project"
+		)
+)
 public class JavaBankProjectApplication {
 
 	public static void main(String[] args) {
