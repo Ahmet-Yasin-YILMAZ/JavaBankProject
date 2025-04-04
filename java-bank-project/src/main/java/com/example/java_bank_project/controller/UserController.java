f917cb2d386538f0a3c5924b3c1e60b20012caeb
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-04-04 22:07:46 +0300

    Updated UserController.java

--- java-bank-project/src/main/java/com/example/java_bank_project/controller/UserController.java
+++ java-bank-project/src/main/java/com/example/java_bank_project/controller/UserController.java
@@ -1,10 +1,10 @@
 package com.example.java_bank_project.controller;
 
-import com.example.java_bank_project.dto.BankResponse;
-import com.example.java_bank_project.dto.CreditDebitRequest;
-import com.example.java_bank_project.dto.EnquiryRequest;
-import com.example.java_bank_project.dto.UserRequest;
+import com.example.java_bank_project.dto.*;
 import com.example.java_bank_project.service.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,16 +12,32 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/user")
+@Tag(name = "User Account Management APIs")
 public class UserController {
 
     private final UserService userService;
-
-
+    @Operation(
+            summary = "Create New User Account",
+            description = "Creating a new user and assigning an account ID"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "Http Status 201 CREATED"
+    )
     @PostMapping
     public BankResponse createAccount(@RequestBody UserRequest userRequest){
         return userService.createAccount((userRequest));
     }
 
+
+    @Operation(
+            summary = "Balance Enquiry",
+            description = "Given an account number, check how such the user has"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Http Status 200 SUCCESS"
+    )
     @GetMapping("/balanceEnquiry")
     public BankResponse balanceEnquiry(@RequestBody EnquiryRequest request){
         return userService.balanceEnquiry(request);
@@ -40,5 +56,10 @@ public class UserController {
     @PostMapping("/debit")
     public BankResponse debitAccount(@RequestBody CreditDebitRequest request){
         return userService.debitAccount(request);
+    }
+
+    @PostMapping("/transfer")
+    public BankResponse transfer(@RequestBody TransferRequest request){
+        return userService.transfer(request);
     }
 }
