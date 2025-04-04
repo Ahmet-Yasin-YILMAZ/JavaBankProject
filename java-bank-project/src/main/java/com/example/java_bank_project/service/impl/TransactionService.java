1c14edad810e7dbf7a3878686961c4de29716403
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-04-04 22:07:01 +0300

    Added TransactionService.java

---
+++ java-bank-project/src/main/java/com/example/java_bank_project/service/impl/TransactionService.java
@@ -0,0 +1,7 @@
+package com.example.java_bank_project.service.impl;
+
+import com.example.java_bank_project.dto.TransactionDto;
+
+public interface TransactionService {
+    void saveTransaction(TransactionDto transactionDto);
+}
