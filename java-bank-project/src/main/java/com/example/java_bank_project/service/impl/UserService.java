e87154ec9867d67c9743d884112e032d5c84068d
Ahmet Yasin YILMAZ <ylmzayasin@gmail.com>
2025-04-06 16:15:25 +0300

    Updated UserService.java

--- java-bank-project/src/main/java/com/example/java_bank_project/service/impl/UserService.java
+++ java-bank-project/src/main/java/com/example/java_bank_project/service/impl/UserService.java
@@ -2,11 +2,15 @@ package com.example.java_bank_project.service.impl;
 
 import com.example.java_bank_project.dto.*;
 
+import java.util.List;
+
 public interface UserService {
     BankResponse createAccount(UserRequest userRequest);
     BankResponse balanceEnquiry(EnquiryRequest request);
     String nameEnquiry(EnquiryRequest request);
-    BankResponse creaditAccount(CreditDebitRequest request);
+    BankResponse creditAccount(CreditDebitRequest request);
     BankResponse debitAccount(CreditDebitRequest request);
     BankResponse transfer(TransferRequest request);
+    public List<UserResponse> getAllUsers();
+    void deleteUser(Long id);
 }
